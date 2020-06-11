93cec05b87e958c4d5661c033fe6f23096b7c8b9
jason-nordheim <jason.nordheim@gmail.com>
2020-06-11 12:59:53 -0600

    added comments to explain functionality

--- src/hooks/useGameStatus.js
+++ src/hooks/useGameStatus.js
@@ -5,10 +5,12 @@ export const useGameStatus = rowsCleared => {
     const [rows, setRows] = useState(0)
     const [level, setLevel] = useState(0)
 
-    const linePoints = [40,100,300,1200]
+    // original tetris scoring 
+    const linePoints = [40, 100 ,300 ,1200] 
 
     const calcScore = useCallback(() => {
-        if ( rowsCleared > 0 ) {
+        // if any rows were cleared, then we can calculate score 
+        if ( rowsCleared > 0 ) { 
             setScore(prev => prev + linePoints[rowsCleared - 1] * (level + 1))
             setRows(prev => prev + rowsCleared)
         }
@@ -16,7 +18,7 @@ export const useGameStatus = rowsCleared => {
 
     useEffect(() => {
         calcScore()
-    }, [calcScore, rowsCleared, score])
+    }, [calcScore, rowsCleared, score]) // dependencies 
 
     return [score, setScore, rows, setRows, setLevel]
 }
