67b079f55c197a3bd2e250f0c9f8e1d9c9e2f8ee
jason-nordheim <jason.nordheim@gmail.com>
2021-01-13 09:14:28 -0700

    added controlsComponent to TetrisComponent

--- src/components/TetrisComponent.tsx
+++ src/components/TetrisComponent.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import BoardComponent from "./BoardComponent";
 import { useTetris } from "../hooks/useTetris";
+import ControlsComponent from "./ControlsComponent";
 
 function TetrisComponent() {
   const tetris = useTetris();
   return (
     <div className="tetris">
       <h1 className="tetris__title">Tetris</h1>
-      <BoardComponent tetris={tetris} />
+      <div className="tetris__container">
+        <BoardComponent tetris={tetris} />
+        <ControlsComponent tetris={tetris} />
+      </div>
     </div>
   );
 }
